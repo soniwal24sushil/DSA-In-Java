6235098e2d76168a78a1379d156796fda062cab5
soniwal24sushil <soniwal24sushil@gmail.com>
2024-01-13 13:00:36 +0530

    1d Array, Array Methods, Linear & Binary Search

---
+++ src/DSA_01_Array/Pack_01_Array_1D/T_02_Array_Methods.java
@@ -0,0 +1,25 @@
+package DSA_01_Array.Pack_01_Array_1D;
+
+import java.util.Arrays;
+
+public class T_02_Array_Methods {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 6, 5, 7, 9, 12};
+
+        // * To calculate the length of the array.
+        int len = arr.length;
+        System.out.println("The length of the array is:- " + len);
+
+        // * To print an array.
+        System.out.println(Arrays.toString(arr));
+
+        // * To sort an array.
+        Arrays.sort(arr);
+
+        // * Copy elements in a given range.
+        int[] temp = Arrays.copyOfRange(arr, 1, 5);
+        System.out.println(Arrays.toString(temp));
+
+    }
+}
